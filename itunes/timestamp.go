43c650be31d1363be5d40f8a8e8c60d924174248
Andriy Oblivantsev <eslider@gmail.com>
2021-06-08 13:42:30 +0200

    Fix return error by timestamp GetDate

--- itunes/timestamp.go
+++ itunes/timestamp.go
@@ -2,18 +2,13 @@ package itunes
 
 import (
 	"database/sql/driver"
-	"log"
 	"time"
 )
 
 type TimeStamp LabelContainer
 
-func (t TimeStamp) GetDate() time.Time {
-	var d, err = time.Parse("2006-01-02T15:04:05-07:00", t.Label)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	return d
+func (t TimeStamp) GetDate() (time.Time, error) {
+	return time.Parse("2006-01-02T15:04:05-07:00", t.Label)
 }
 
 func (t TimeStamp) Value() (driver.Value, error) {
